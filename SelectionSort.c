8d5c63063d2b77174772fc81a5c2fa30b81c8933
Pranit <pranitsinha@protonmail.com>
2022-11-01 19:35:23 +0530

    corrected code

--- SelectionSort.c
+++ SelectionSort.c
@@ -1,22 +1,10 @@
 #include <stdio.h> 
 #include <stdlib.h> 
 
-int * generateArray(int n) {
-    int a[n];
+void fillArray(int * a, int n) {
     for (int i = 0; i < n; i++) {
         printf("Enter element in position %d: ", i);
         scanf("%d", &a[i]);
-    }
-    return a;
-}
-
-void selectionSort (int * a, int n) {
-    int max, temp;
-    for (int i = n; i>1; i=i-1) {
-        max     = findIndexOfMax(a, i);
-        temp    = a[max];
-        a[max]  = a[i-1];
-        a[i-1]  = temp;
     }
 }
 
@@ -32,17 +20,28 @@ int findIndexOfMax (int * a, int n) {
     return max;
 }
 
-void printArray(int * a, int n) {
-    printf("%d", &a[0]);
-    for (int i = 1; i<n; i++) {
-        printf(", %d", &a[i]);
+void selectionSort (int * a, int n) {
+    int max, temp;
+    for (int i = n; i>1; i=i-1) {
+        max     = findIndexOfMax(a, i);
+        temp    = a[max];
+        a[max]  = a[i-1];
+        a[i-1]  = temp;
     }
 }
 
-int main (int argc, char** argv[]) {
+void printArray(int * a, int n) {
+    printf("%d", a[0]);
+    for (int i = 1; i<n; i++) {
+        printf(", %d", a[i]);
+    }
+}
+
+int main (int argc, char** argv) {
     int n;
-    printf("Enter number of elements in array.");
+    printf("Enter number of elements in array. ");
     scanf("%d",&n);
-    int * a = generateArray(n);
+    int a[n];
+    fillArray(a, n);
     printArray(a, n);
 }
