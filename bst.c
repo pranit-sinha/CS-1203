e30bdc203ed3cd83c8fcecebaafaa95b0781a0f9
Pranit <pranitsinha@protonmail.com>
2022-11-15 12:37:34 +0530

    this code produces a segfault

--- bst.c
+++ bst.c
@@ -10,22 +10,32 @@ struct node {
 
 typedef struct node * nodeAddress;
 
-//void generateBstFromArray(a, n) {
-//    nodeAddress root = NULL;
-//    int current = 0;
-//    if (n>0) {
-//       root->val = a[current];
-//    }
-//}
+nodeAddress generateBSTFromArray(int * arr, int a, int b, nodeAddress root) {
+    if(a>b) { return NULL;}
+    int c = (a+b)/2;
+    if (root==NULL) {
+        root->val = arr[c];
+    }
+    root->left = generateBSTFromArray(arr, a, c-1, root->left);
+    root->right = generateBSTFromArray(arr, c+1, b, root->right);
+    return root;
+}
+
+void printBST(nodeAddress root, int height) {
+    if (root == NULL) {return;}
+    printf("%d\n", root->val);
+    printBST(root->left, height + 1);
+    printBST(root->right, height + 1);
+}
  
 int main (int argc, char** argv) { 
     int n = inputSizeArray();
-    int a[n];
-    fillArray(a, n);
-    selectionSort(a, n);
-    //generateBstFromArray(a, n);
-    printArray(a, n);
-    //printBST;
+    int arr[n];
+    fillArray(arr, n);
+    selectionSort(arr, n);
+    nodeAddress root = NULL;
+    root = generateBSTFromArray(arr, 0, n-1, root);
+    printBST(root, 0);
 }
 
 
