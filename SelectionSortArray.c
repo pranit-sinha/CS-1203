4cfe48f874a975da66b0e684c26470fd8abaf5fb
Pranit <pranitsinha@protonmail.com>
2022-11-25 18:27:20 +0530

    Fixed selection sort using addresses

--- SelectionSortArray.c
+++ SelectionSortArray.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 #include "headers/Array.h"
 
-int findAddressOfMax (int * a, int n) {
+int * findAddressOfMax (int * a, int n) {
     int * maxAddress = NULL;
     if (n>=1) { 
         maxAddress = a;
-        for (int i = 1; i<n; i++) {
+        for (int i = 0; i<n; i++) {
             if ( *(a+i) > *maxAddress ) {
                 maxAddress = (a+i);
             }
@@ -15,16 +15,18 @@ int findAddressOfMax (int * a, int n) {
     }
 }
 
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
 void selectionSort (int * a, int n) {
     int * maxAdd;
-    int * temp;
-    for (int i = n; i>1; i=i-1) {
+    int temp;
+    for (int i = n; i>0; i=i-1) {
         maxAdd  = findAddressOfMax(a, i);
-        temp = maxAdd;
-        *maxAdd   = a[i-1];
-        a[i-1]  = *temp;
-        printf("The max element is %p\n", maxAdd);
-       // printf("The max element is %d\n", temp);
+        swap(maxAdd, &a[i-1]);
     }
 }
 
