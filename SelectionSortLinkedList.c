bdb3840d005b5d430468260ef356f52f740af949
Pranit <pranitsinha@protonmail.com>
2022-12-13 00:34:55 +0530

    filled in code for selection sort on linked lists

--- SelectionSortLinkedList.c
+++ SelectionSortLinkedList.c
@@ -2,9 +2,29 @@
 #include <stdlib.h> 
 #include "headers/LinkedList.h"
 
+void selectionSort(nodeAddress head) {
+    nodeAddress temp = head;
+    while (temp) {
+        nodeAddress min    = temp;
+        nodeAddress rem    = temp->next;
+        while (rem) {
+            if (min->val > rem->val) {
+                min = rem;
+            }
+            rem = rem->next;
+        }
+
+        int t       = temp->val;
+        temp->val   = min->val;
+        min->val    = t;
+        temp        = temp->next;
+    }
+}
+
 int main (int argc, char** argv) { 
-    int n = inputSize();
+    int n = input();
     nodeAddress headOfList = generateLinkedList(n);
+    selectionSort(headOfList);
     printLinkedList(headOfList);
     freeLinkedList(headOfList);
 }
